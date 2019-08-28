8037ac3cc641d6ad014541f362336a344fc4c2b9
royramstad <royramstad@outlook.com>
2019-08-28 12:33:00 -0700

    Create New Robofriends Redux

---
+++ robofriends redux/src/Components/Robots.js
@@ -0,0 +1,50 @@
+export const Robots = [
+    {
+        id: 1,
+        name: 'Gonzalo',
+        username: 'SoccerTennisChamp01',
+        email: 'gonzalo@soccershots.org'
+    },
+    {
+        id: 2,
+        name: 'Victor',
+        username: 'ElFalcon',
+        email: 'vfalcon@soccershots.org'
+    },
+    {
+        id: 3,
+        name: 'Ashlynne',
+        username: 'PieFace',
+        email: 'ahuon@soccershots.org'
+    },
+    {
+        id: 4,
+        name: 'Jacob',
+        username: 'ShrekIsLove',
+        email: 'jkappes@soccershots.org'
+    },
+    {
+        id: 5,
+        name: 'Taylor',
+        username: 'TSwitty',
+        email: '???'
+    },
+    {
+        id: 6,
+        name: 'Alexis',
+        username: 'MySisIsCray',
+        email: '???'
+    },
+    {
+        id: 7,
+        name: 'Sarah',
+        username: 'ShrekIsLyfe',
+        email: '???'
+    },
+    {
+        id: 8,
+        name: 'Roy',
+        username: 'FrickinIdiot',
+        email: 'royramstad@outlook.com'
+    }
+]
